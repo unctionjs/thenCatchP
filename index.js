bfc25be84a6ee1b57261d5cab26daffbe5abb0da
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export default function thenCatchP (resolution: any => any): Function {
-  return function thenCatchPResolution (rejection: any => any): Function {
-    return function thenCatchPResolutionRejection (promise: Promise<any>): Promise<any> {
-      return promise.then(resolution, rejection)
-    }
-  }
+export default function thenCatchP(resolution) {
+  return function thenCatchPResolution(rejection) {
+    return function thenCatchPResolutionRejection(promise) {
+      return promise.then(resolution, rejection);
+    };
+  };
 }
